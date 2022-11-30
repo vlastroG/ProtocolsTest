66576baf314ecb32971e8c33c7aa4854a2639b3d
Vladimir Stroganov <vlastroganov@gmail.com>
2022-11-30 22:40:56 +0300

    Добавить базовые модели и подключить Newtonsoft.Json

---
+++ LibraryService/LibraryService/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryService.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+
+        public string Language { get; set; }
+
+
+        public override string ToString() => $"{Name} ({Language})";
+    }
+}
---
+++ LibraryService/LibraryService/Services/Impls/LibraryDatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using LibraryService.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace LibraryService.Services.Impls
+{
+    public class LibraryDatabaseContext : ILabraryDatabaseContextService
+    {
+        private IList<Book> _libraryDatabase;
+
+        public IList<Book> Books => throw new NotImplementedException();
+
+        public LibraryDatabaseContext()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+    }
+}

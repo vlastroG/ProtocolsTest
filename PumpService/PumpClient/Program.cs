7ae1c6bf46d368d3897cae8060be90322d090d03
Vladimir Stroganov <vlastroganov@gmail.com>
2022-12-04 19:56:46 +0300

    Сделать базовую реализацию клиента

--- PumpService/PumpClient/Program.cs
+++ PumpService/PumpClient/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PumpClient.PumpServiceReference;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace PumpClient
     {
         static void Main(string[] args)
         {
+            InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+            PumpServiceClient client = new PumpServiceClient(instanceContext);
+
+            client.UpdateAndCompileScript("C:\\Users\\vlast\\source\\repos\\ProtocolsTest\\PumpService\\PumpService\\Scripts\\Sample.script");
+            client.RunScript();
+
+            Console.ReadKey(true);
+            client.Close();
         }
     }
 }

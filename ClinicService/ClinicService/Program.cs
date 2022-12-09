d9734efeca1c35bf39fc199be57c143f4d652e7c
Vladimir Stroganov <vlastroganov@gmail.com>
2022-12-09 22:24:29 +0300

    Добавление первой миграции

--- ClinicService/ClinicService/Program.cs
+++ ClinicService/ClinicService/Program.cs
@@ -1,3 +1,6 @@
+using ClinicService.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace ClinicService
 {
     public class Program
@@ -7,6 +10,15 @@ namespace ClinicService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            #region Configure EF DB Context Service (ClinicService Database)
+
+            builder.Services.AddDbContext<ClinicServiceDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration["Settings:DatabaseOptions:ConnectionString"]);
+            });
+
+            #endregion
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

9e9f27ca6ae1dfde6121989f8eee1e3cc664cb55
Vladimir Stroganov <vlastroganov@gmail.com>
2022-12-19 23:43:31 +0300

    Подключение необходимых зависимостей, сервисов и пакетов

--- ClinicService/ClinicServiceDotNet7/Program.cs
+++ ClinicService/ClinicServiceDotNet7/Program.cs
@@ -1,3 +1,8 @@
+using ClinicService.Data.DbContexts;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
 namespace ClinicServiceDotNet7
 {
     public class Program
@@ -6,18 +11,51 @@ namespace ClinicServiceDotNet7
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+            #region Configure Kestrel
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Any, 5100, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http2;
+                });
+                options.Listen(IPAddress.Any, 5101, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http1;
+                });
+            });
+            #endregion
+
+            #region Configuration gRPC
+
+            builder.Services.AddGrpc()
+                .AddJsonTranscoding();
+
+            #endregion
+
+
+            #region Configure EF DB Context Service (ClinicService Database)
+            builder.Services.AddDbContext<ClinicServiceDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration["Settings:DatabaseOptions:ConnectionString"]);
+            });
+            #endregion
+
+
+
             // Add services to the container.
             builder.Services.AddAuthorization();
 
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            app.UseRouting();
+            app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
 
-            app.UseAuthorization();
-
-
-
+            app.MapGrpcService<Services.Impls.ClinicService>().EnableGrpcWeb();
+            app.MapGet("/",
+                () =>
+                "Communication with gRPC endpoints must be made through a gRPC client.");
 
             app.Run();
         }

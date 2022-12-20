15d0e987b80a95cd69057287c191f0c8c5ded1f2
Vladimir Stroganov <vlastroganov@gmail.com>
2022-12-21 00:06:46 +0300

    Сделать возможным http и grpc содинение

--- ClinicService/ClinicServiceDotNet7/Program.cs
+++ ClinicService/ClinicServiceDotNet7/Program.cs
@@ -1,6 +1,7 @@
 using ClinicService.Data.DbContexts;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Net;
 
 namespace ClinicServiceDotNet7
@@ -39,15 +40,36 @@ namespace ClinicServiceDotNet7
             {
                 options.UseSqlServer(builder.Configuration["Settings:DatabaseOptions:ConnectionString"]);
             });
+
+            #endregion
+
+
+            #region Congifure Swagger
+
+            builder.Services.AddGrpcSwagger();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo { Title = "ClinicService", Version = "v1" });
+
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "ClinicServiceDotNet7.xml");
+                c.IncludeXmlComments(filePath);
+                c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+            });
+
             #endregion
 
 
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-
             var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
+            }
 
             app.UseRouting();
             app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
